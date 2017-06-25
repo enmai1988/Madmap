b7cbef0b814d4b6583844bd2682c8d953402e4db
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 14:13:05 -0700

    fixing the local deploy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
-
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+process.env.PWD = process.cwd();
+app.use(express.static(process.env.PWD + '/react-client/dist'));
+
+// app.get('/', function(req, res) {
+//   res.send('hello world!');
+// });
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist'); 
-
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//plugin in loaders an array, call new html
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',      
         query: {
           presets: ['react', 'es2015', 'stage-0']
-       }
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html 
+      template: 'react-client/src/index.html'
+    })
+  ]
 };
