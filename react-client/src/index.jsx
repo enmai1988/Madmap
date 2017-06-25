6e49f8a8f83d48374a3ef63af940ba373174ef1b
Alexander Poet <alex.a.poet@gmail.com>
2017-06-25 11:31:03 -0700

    2nd block changes

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,9 +2,9 @@ import React, { Component }from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MainMap from './components/map.jsx'
-import PinCreator from './components/pincreator.jsx'
-import PinInfo from './components/pininfo.jsx'
+import MainMap from './components/map.jsx';
+import PinCreator from './components/pincreator.jsx';
+import PinInfo from './components/pininfo.jsx';
 import Header from './components/header.jsx'; 
 // import axios from 'axios';
 
