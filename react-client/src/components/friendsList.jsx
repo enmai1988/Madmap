161631fdc366343a224f065e3938840e7d313269
David Vassett <david@davidvassett.com.au>
2017-07-06 17:41:45 -0700

    add friend list component

---
+++ react-client/src/components/friendsList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { List, ListItem } from 'material-ui/List';
+import AutoComplete from 'material-ui/AutoComplete';
+import Avatar from 'material-ui/Avatar';
+import Divider from 'material-ui/Divider';
+import Subheader from 'material-ui/Subheader';
+
+export default class FriendsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="friend_list">
+        <List>
+          <Subheader>My Friends</Subheader>
+          {this.props.friends.map((friend, index) => {
+            return <ListItem
+              leftAvatar={<Avatar src={friend.avatar} />}
+              primaryText={friend.emailAddress}
+              key={'A' + index} />;
+          })}
+        </List>
+      </div>
+    );
+  }
+}
--- react-client/src/components/userpage.jsx
+++ react-client/src/components/userpage.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import FriendsList from './friendsList.jsx';
 
 export default class UserPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      friends: [
+        {
+          id: 1,
+          emailAddress: 'david@elovate.io',
+          avatar: 'http://placehold.it/50x50'
+        },
+        {
+          id: 2, 
+          emailAddress: 'david-1@elovate.io',
+          avatar: 'http://placehold.it/50x50'
+        }
+      ]
     };
   }
 
   render() {
     return (
-      <p>Hey What's up Hello</p>
+      <FriendsList friends={this.state.friends} />
     );
   }
 }
